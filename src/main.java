5dce966715b7408282c9778b40b060962dde2cd7
Dennis <duysak92@gmail.com>
2017-04-12 19:35:06 +0200

    Initialimplementierung der harten Restriktionen

--- src/main.java
+++ src/main.java
@@ -1,18 +1,23 @@
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import attributes.*;
-// hallo ab
 
 public class main {
     public static void main(String argv[]) throws ParseException {
         XMLReader xmlReader = new XMLReader();
+        //Problem
         SchedulingPeriod schedulingPeriod = xmlReader.parseXML("long01");
         InitialSolution initialSolution = new InitialSolution(schedulingPeriod);
+        //Initiallösung:
         List<int[][]> initial = initialSolution.createSolution();
+
+        HardRest hardRest = new HardRest(schedulingPeriod);
+        boolean  b = hardRest.checkHardRest(initial);
+
+
+
+
     }
 }
