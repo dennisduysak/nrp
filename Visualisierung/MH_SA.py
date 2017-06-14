fdf25b5a6863cb3da51cf948445371d4ef5021a2
d-schmitt <daniela.schmitt@mailbox.org>
2017-06-14 11:35:14 +0200

    Anpassungen für Sim1
    Struktur ist mist, aber ging so schnell :)

--- Visualisierung/MH_SA.py
+++ Visualisierung/MH_SA.py
@@ -10,7 +10,7 @@ https://matplotlib.org/users/pyplot_tutorial.html
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_name = 'output'
+file_name = 'output_new'
 
 # Load Data DataFrame
 mh_sa = pd.read_csv(file_name+'.csv', sep=',')
@@ -18,26 +18,27 @@ mh_sa = pd.read_csv(file_name+'.csv', sep=',')
 # Ziel: Darstellung des Scores in Abhängigkeit der Cooling Rate und Starttemperatur (jeweils was sich ändert)
 # insgesamt 5 Grafiken
 # Startemperatur konstant
-Dateiname = list(set(mh_sa.Dateiname.tolist()))
+#Dateiname = list(set(mh_sa.Dateiname.tolist()))
+
+Dateiname = ['toy1', 'long01','long_hidden01',  'long_hint01','long_late01',
+             'medium01', 'medium_hidden01','medium_hint01','medium_late01','sprint01',
+             'sprint_hidden01','sprint_hint01','sprint_late01']
+# Anzeige für Dateidurchlaufen
+i = 0
+j = 10
+
 
 for dateiname in Dateiname:
     
     #### Für feste Starttemperatur
-    i = 0
-    j = 10
     plt.figure(figsize=(15,4))
-    a = 151
+    a = 141
     b = 1
     
-    while(b<6): # j<100
-        
-        #plt.figure(figsize=(4,4))
+    while(b<4): 
         plt.subplot(a)
-        #plt.set_ybound(20,23)
         grafik = plt.plot(mh_sa.CoolingRate[i:j], mh_sa.Score[i:j], label = [mh_sa.Dateiname[i], mh_sa.StartTemperatur[i]])
         plt.setp(grafik, color='r', linewidth = 2.0)
-        #toy1 = plt.plot(mh_sa.Iteration, mh_sa.Long01, label = 'Long01.1')
-        
         plt.ylabel('Score')
         plt.xlabel("CoolingRate")
         plt.ylim(mh_sa[mh_sa.Dateiname == dateiname].Score.min(),mh_sa[mh_sa.Dateiname == dateiname].Score.max())
@@ -46,29 +47,90 @@ for dateiname in Dateiname:
         j = j+20
         a = a+1
         b = b+1
-    
+        plt.gca().invert_xaxis()
+      
+    plt.savefig(dateiname+'1_fig.png', dpi=800)
     plt.show()
-    #plt.savefig(file_name+'_fig.png', dpi=800)
     
-    # Cooling Rate konstant
-    """ 
-    i = 10
-    j = 20
-    while(j <= len(mh_sa)): # j<100
-        plt.figure(figsize=(4,4))
-         
-        grafik = plt.plot(mh_sa.StartTemperatur[i:j], mh_sa.Score[i:j], label = [mh_sa.Dateiname[i], mh_sa.CoolingRate[i]])
-        plt.setp(grafik, color='r', linewidth = 2.0)
-        #toy1 = plt.plot(mh_sa.Iteration, mh_sa.Long01, label = 'Long01.1')
+    plt.figure(figsize=(15,4))
+    a = 141
+    b = 1
+    
+    while(b<4): 
         
+        plt.subplot(a)
+        grafik = plt.plot(mh_sa.CoolingRate[i:j], mh_sa.Score[i:j], label = [mh_sa.Dateiname[i], mh_sa.StartTemperatur[i]])
+        plt.setp(grafik, color='r', linewidth = 2.0)
         plt.ylabel('Score')
         plt.xlabel("CoolingRate")
-        
+        plt.ylim(mh_sa[mh_sa.Dateiname == dateiname].Score.min(),mh_sa[mh_sa.Dateiname == dateiname].Score.max())
         plt.legend()
-        plt.show()
         i = j+10
         j = j+20
-       """ 
-
-#mh_sa[mh_sa.Dateiname == 'toy1'].describe()
-
+        a = a+1
+        b = b+1
+        plt.gca().invert_xaxis()
+    
+    plt.savefig(dateiname+'2_fig.png', dpi=800)
+    plt.show()
+    
+    
+    
+    ##########################################################################
+    # Cooling Rate fest, Starttemperatur variabel
+i = 10
+j = 20
+for dateiname in Dateiname:
+    
+    plt.figure(figsize=(15,4))
+    a = 141
+    b = 1 
+    
+    while(b<4): 
+        
+        plt.subplot(a)
+        grafik = plt.plot(mh_sa.StartTemperatur[i:j], mh_sa.Score[i:j], label = [mh_sa.Dateiname[i], mh_sa.CoolingRate[i]])
+        plt.setp(grafik, color='r', linewidth = 2.0)
+        
+        plt.ylabel('Score')
+        plt.xlabel("StartTemperatur")
+        plt.ylim(mh_sa[mh_sa.Dateiname == dateiname].Score.min(),mh_sa[mh_sa.Dateiname == dateiname].Score.max())
+        plt.legend()
+        i = j+10
+        j = j+20
+        a = a+1
+        b = b+1
+        plt.gca().invert_xaxis()
+    
+    plt.savefig(dateiname+'ST_fig.png', dpi=800)
+    plt.show()
+    
+    plt.figure(figsize=(15,4))
+    a = 141
+    b = 1
+    
+    while(b<4):
+        
+        plt.subplot(a)
+        grafik = plt.plot(mh_sa.StartTemperatur[i:j], mh_sa.Score[i:j], label = [mh_sa.Dateiname[i], mh_sa.CoolingRate[i]])
+        plt.setp(grafik, color='r', linewidth = 2.0)
+        plt.ylabel('Score')
+        plt.xlabel("StartTemperatur")
+        plt.ylim(mh_sa[mh_sa.Dateiname == dateiname].Score.min(),mh_sa[mh_sa.Dateiname == dateiname].Score.max())
+        plt.legend()
+        i = j+10
+        j = j+20
+        a = a+1
+        b = b+1
+        plt.gca().invert_xaxis()
+    
+    plt.savefig(dateiname+'ST2_fig.png', dpi=800)
+    plt.show()
+    
+      
+"""
+Ideen:
+    - einmal mit gleichbleibender Starttemperatur und sich ändernder Cooling Rate von 10 - 0.002
+    - 
+"""
+  
